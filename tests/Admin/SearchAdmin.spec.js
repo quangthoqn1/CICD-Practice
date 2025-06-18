c6531afdd0564802b9f1dd7a87cf983dde1dd18f
Thọ Ngô <56387461+quangthoqn1@users.noreply.github.com>
2025-06-18 14:08:47 +0700

    Admin

--- tests/Admin/SearchAdmin.spec.js
+++ tests/Admin/SearchAdmin.spec.js
@@ -7,13 +7,10 @@ test('Search Admin at Admin Page', async ({ page }) => {
   await loginPage.goto();
   await loginPage.login('Admin', 'admin123');
   await loginPage.assertUrlAfterLogin();
-  await page.waitForTimeout(2000);
   const adminPage = new AdminPage(page);
   await adminPage.NavigateToAdmin();
   await adminPage.InputUserName('Admin');
-  await page.waitForTimeout(2000);
   await adminPage.SelectUserDropdownList();
-  await page.waitForTimeout(2000);
   await adminPage.InputEmployeeName('#@!! user');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(3000);
 });
